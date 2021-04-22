8db55f6a381463fe966f5f1a944cd2d2d9f43560
Américo MQ <americo.meneses.contractor@bbva.com>
2021-04-22 15:55:38 -0500

    first commit

--- testiOS/Controllers/LoginViewController.swift
+++ testiOS/Controllers/LoginViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  LoginViewController.swift
 //  testiOS
 //
 //  Created by IDS Comercial on 22/04/21.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
---
+++ testiOS/Helpers/Constants.swift
@@ -0,0 +1,8 @@
+//
+//  Constants.swift
+//  testiOS
+//
+//  Created by IDS Comercial on 22/04/21.
+//
+
+import Foundation
