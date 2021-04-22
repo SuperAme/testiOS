24fc37d373232b1ec48b44ea3c7950a6af686745
Américo MQ <americo.meneses.contractor@bbva.com>
2021-04-22 16:05:47 -0500

    adding basic Views

---
+++ testiOS/Controllers/DashboardViewController.swift
@@ -0,0 +1,15 @@
+//
+//  DashboardViewController.swift
+//  testiOS
+//
+//  Created by IDS Comercial on 22/04/21.
+//
+
+import Foundation
+import UIKit
+
+class DashboardViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ testiOS/Controllers/RegisterViewController.swift
@@ -0,0 +1,16 @@
+//
+//  RegisterViewController.swift
+//  testiOS
+//
+//  Created by IDS Comercial on 22/04/21.
+//
+
+import Foundation
+import UIKit
+
+class RegisterViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
