1ae2140ab9de4b33a68b0049d4dd9894e1bbe613
Américo MQ <americo.meneses.contractor@bbva.com>
2021-04-23 00:50:30 -0500

    addind alerts

--- testiOS/Controllers/DashboardViewController.swift
+++ testiOS/Controllers/DashboardViewController.swift
@@ -9,11 +9,71 @@ import Foundation
 import UIKit
 
 class DashboardViewController: UIViewController {
+    @IBOutlet weak var segmentedCtrl: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-    @IBAction func addPressed(_ sender: UIBarButtonItem) {
-//        AlertView.instance.showAlert(title: "master", message: "Again", alertType: .failure)
-        registerAlert.instance.showAlert()
+    
+    @IBAction func segmentedControlPressed(_ sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex {
+        case 0:
+            break;
+        case 1:
+            break;
+        case 2:
+            showPlusAlert()
+        case 3:
+            showMinusAlert()
+        default:
+            break;
+        }
+    }
+    
+    func showPlusAlert() {
+        let alert = UIAlertController(title: "Agrega tu ingreso", message: "", preferredStyle: .alert)
+        alert.addTextField { (textField) in
+            textField.placeholder = "Ingresa cantidad en numero"
+        }
+        alert.addTextField { (textField) in
+            textField.placeholder = "Ingresa Descripción"
+        }
+        
+        alert.addAction(UIAlertAction(title: "Cancelar", style: .cancel, handler: { (action) in
+            self.segmentedCtrl.selectedSegmentIndex = 0
+            self.dismiss(animated: true, completion: nil)
+        }))
+        
+        alert.addAction(UIAlertAction(title: "Agregar", style: .default, handler: { [weak self] _ in
+            guard let field = alert.textFields?[0], let field2 = alert.textFields?[1], let text = field.text, let text2 = field2.text , !text.isEmpty else {
+                return
+            }
+            self!.segmentedCtrl.selectedSegmentIndex = 0
+            print(text+"++"+text2)
+        }))
+        present(alert, animated: true)
+    }
+    func showMinusAlert() {
+        let alert = UIAlertController(title: "Agrega tu gasto", message: "", preferredStyle: .alert)
+        alert.addTextField { (textField) in
+            textField.placeholder = "Ingresa cantidad en numero"
+        }
+        alert.addTextField { (textField) in
+            textField.placeholder = "Ingresa Descripción"
+        }
+        
+        alert.addAction(UIAlertAction(title: "Cancelar", style: .cancel, handler: { (action) in
+            self.segmentedCtrl.selectedSegmentIndex = 0
+            self.dismiss(animated: true, completion: nil)
+        }))
+        
+        alert.addAction(UIAlertAction(title: "Agregar", style: .default, handler: { [weak self] _ in
+            guard let field = alert.textFields?[0], let field2 = alert.textFields?[1], let text = field.text, let text2 = field2.text , !text.isEmpty else {
+                return
+            }
+            self!.segmentedCtrl.selectedSegmentIndex = 0
+            print(text+"++"+text2)
+        }))
+        present(alert, animated: true)
     }
 }
